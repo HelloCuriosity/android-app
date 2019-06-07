b8a089c99e2b7312d1fbf687a071884ee859e615
Kyle Roe <kyle.roe@apertomove.de>
2019-06-07 10:05:01 +0200

    initial commit

---
+++ app/src/test/java/com/hello/curiosity/ExampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.hello.curiosity
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun testExampleTest() {
+        assertEquals(4, 2 + 2)
+    }
+}
