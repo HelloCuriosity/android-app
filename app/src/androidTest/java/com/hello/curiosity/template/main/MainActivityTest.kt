06d1471076451cd2decb6b27f4950417d66ffd76
Kyle <hopeman1986@gmail.com>
2020-01-25 00:14:31 +0100

    updated MainActivityTest

--- app/src/androidTest/java/com/hello/curiosity/template/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/hello/curiosity/template/main/MainActivityTest.kt
@@ -1,11 +1,13 @@
 package com.hello.curiosity.template.main
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.hello.curiosity.R
 import com.hello.curiosity.template.test.EspressoAssert.Companion.assertView
 import com.hello.curiosity.template.test.isVisible
 import com.hello.curiosity.template.test.withText
+import junit.framework.Assert.assertNotNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,6 +20,11 @@ class MainActivityTest {
 
     @Test
     fun testActivity() {
+        val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = MainActivity.createIntent(targetContext)
+        val activity = rule.launchActivity(intent)
+        assertNotNull(activity)
+
         assertView(R.id.tv_main)
             .isVisible()
             .withText("Hello World!")
